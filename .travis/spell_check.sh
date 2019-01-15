36cf55cdb953af1c163de1f89bb4525411c3cbda
Guy Taylor <thebigguy.co.uk@gmail.com>
2019-01-15 23:25:07 +0000

    Fix up Travis spell checking

--- .travis/spell_check.sh
+++ .travis/spell_check.sh
@@ -6,6 +6,15 @@ set -o pipefail
 
 FILE_TO_CHECK="${1}"
 
-echo "Spelling check ${FILE_TO_CHECK} ..."
-aspell --lang='en_GB' --home-dir="$(pwd)" --mode='html' --add-html-skip="githash" pipe < "${FILE_TO_CHECK}" | grep '[^*]' | grep -v '^@(#) ' | tee >(grep -v '.*')
-echo "... done"
+echo -n "Spelling check ${FILE_TO_CHECK} ... "
+result=$(cat "${FILE_TO_CHECK}" | aspell --lang='en_GB' --home-dir="$(pwd)" --mode='html' --add-html-skip="githash" pipe | sed -E '/^[^&#]/d' | sed '/^$/d')
+if echo "${result}" | grep -E '^[&#]' > /dev/null ; then
+  echo "FAIL"
+  echo '###############################################################################'
+  echo "${result}" | sed -e 's/^& //g'
+  echo '###############################################################################'
+  exit 1
+else
+  echo "OK"
+  exit 0
+fi
