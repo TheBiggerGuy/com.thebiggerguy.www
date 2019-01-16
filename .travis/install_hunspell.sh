bfddc7aec0ab54fc016f852699697b4fedcd655c
Guy Taylor <thebigguy.co.uk@gmail.com>
2019-01-16 16:10:48 +0000

    Cache Hunspell between Travis builds

--- .travis/install_hunspell.sh
+++ .travis/install_hunspell.sh
@@ -6,6 +6,21 @@ set -o pipefail
 
 cd hunspell
 
+if which hunspell > /dev/null ; then
+  echo 'Hunspell already found on the system'
+
+  GIT_VERSION="$(git describe --tags)"
+  CACHED_VERSION="v$(hunspell --version | head --lines=1 | sed -E 's/.*Hunspell ([0-9]+.[0-9]+.[0-9]+).*/\1/g')"
+
+  echo "GIT_VERSION=${GIT_VERSION} and CACHED_VERSION=${CACHED_VERSION}"
+  if [ "${GIT_VERSION}" = "${CACHED_VERSION}" ]; then
+    echo "Cached/Installed version equal to git version"
+    exit 0
+  else
+    echo "Cached/Installed version NOT equal to git version"
+  fi
+fi
+
 autoreconf -vfi
 ./configure --prefix="${HOME}/.local"
 make
