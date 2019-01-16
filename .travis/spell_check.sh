821da58d4613e2435bcb092360dcb6abc78fedf7
Guy Taylor <thebigguy.co.uk@gmail.com>
2019-01-16 01:59:48 +0000

    Log extra debug info

--- .travis/spell_check.sh
+++ .travis/spell_check.sh
@@ -6,8 +6,6 @@ set -o pipefail
 
 FILE_TO_CHECK="${1}"
 
-export DICPATH="$(pwd)/libreoffice_dictionaries/en"
-
 echo -n "Spelling check ${FILE_TO_CHECK} ... "
 result=$(hunspell -d='en_GB' -p "$(pwd)/.hunspell_default" -H -i 'UTF-8' -a "${FILE_TO_CHECK}" | sed -E '/^[^&#]/d' | sed '/^$/d')
 if echo "${result}" | grep -E '^[&#] ' > /dev/null ; then
