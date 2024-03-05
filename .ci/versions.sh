68a06c23718a43b5a2784c2993ceee411d62d5ea
Guy Taylor <thebigguy.co.uk@gmail.com>
2024-03-05 10:02:04 +0000

    Update Hugo
    Update Hugo to 0.123.7 to keep up with changes. There seems to be no
    issues affecting this site in the changelog.
    
    https://github.com/gohugoio/hugo/releases/tag/v0.123.7
    https://github.com/gohugoio/hugo/compare/v0.123.4...v0.123.7

--- .ci/versions.sh
+++ .ci/versions.sh
@@ -4,5 +4,5 @@ set -o verbose
 set -o errexit
 set -o pipefail
 
-HUGO_VERSION='0.123.4'
-HUGO_HASH='ed1f732d21d5ae83cc18743548e772131797cfc3c249abdc22946aa5816a7e2a'
+HUGO_VERSION='0.123.7'
+HUGO_HASH='928a767c403393f6abb204b69c27489abc78816dafd80901eb9a9f5f835972bc'
